9933c3ee9ee0e1ced0e700b57c53eeb403a8dd36
inikiforov <ievgen.nikiforov@tedata.de>
2025-09-09 12:05:15 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ export const App = () => {
     if (!titleInput) {
       setErrorTitle(true);
     }
+    if (userID !== 0 && titleInput) {
     setTodos([
       ...todos,
       {
@@ -30,6 +31,7 @@ export const App = () => {
     ]);
     setTitleInput('');
     setUserID(0);
+  }
   };
   const selectUser = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setErrorUser(false);
